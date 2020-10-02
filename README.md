d7664840ac929a059d90ee715cb0bb2ae94df056
leomiranda-tc <leonardo.miranda@tradersclub.com.br>
2020-10-02 12:51:48 -0300

    grpc client golang

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # GRPC Golang to Node.js
 In this repository you will find two applications communicating with each other over gRPC.
+
+On MacOs, install protobuf.
+``` shell
+$ brew install protobuf
+```
+
+## Node Server
+Dependencies:
+- @grpc/grpc-js ;
+- @grpc/proto-loader ;
+
+
+## Golang Client
+Dependencies:
+- google.golang.org/grpc ;
+
+To install protofile you need install first protoc-gen-go, like:
+``` shell
+$ brew install protoc-gen-go
+```
+
+To install protofile and create client, run on terminal
+```shell
+$ protoc -I . posts_service.proto --go_out=plugins=grpc:./go-client/services
+```
---
+++ go-client/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	services "github.com/leomirandadev/grpc-project/services"
+	"google.golang.org/grpc"
+)
+
+var (
+	serverURL = "localhost:10000"
+)
+
+func getGRPCClient() *grpc.ClientConn {
+	var opts = []grpc.DialOption{grpc.WithInsecure(), grpc.WithBlock()}
+	conn, err := grpc.Dial(serverURL, opts...)
+
+	if err != nil {
+		log.Fatal("Fail to dial %v", err)
+	}
+
+	return conn
+}
+
+func main() {
+	conn := getGRPCClient()
+
+	defer conn.Close()
+
+	client := services.NewPostServiceClient(conn)
+
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	posts, err := client.GetPosts(ctx, &services.Empty{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, post := range posts.GetPosts() {
+		fmt.Println(post.Id)
+		fmt.Println(post.Title)
+		fmt.Println(post.Text)
+	}
+
+}
